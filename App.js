eb4f1ad7e27cb7a0e2fa22b9f57c4157d9946eca
testUser <shubh_tiwari@hotmail.com>
2021-01-07 02:35:43 +0530

    counter app

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
+//Import 
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+ 
 
-export default function App() {
+//Create custom components
+const App = () => {
+
+  //Declare variable
+  //var counter = 0;
+  const [counter, setCounter] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Current Count : {counter}</Text>
+      <Button title="Increment"
+      onPress={()=>{
+        //counter++;
+        setCounter(counter + 1);
+        console.log(counter);
+      }}
+      />
+      <Button title="Decrement"
+      onPress={()=>{
+        //counter--;
+        setCounter(counter - 1);
+        console.log(counter);
+      }}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//Export 
+export default App;
+
+//Apply Styling
 const styles = StyleSheet.create({
   container: {
     flex: 1,
